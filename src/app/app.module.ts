dd108763798280de3511cb52d078d1c3a2980141
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-01-23 19:58:01 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { Product } from '../model/product';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+title!:string
+listProduct!:Product[]
+color!:string
+
+ngOnInit(){
+  this.title="Welcome 2CINFO1"
+  this.color="";
+  this.listProduct=[
+    {
+      id:1,
+      title:"Iphone 12",
+      price:200,
+      quantity:2,
+      like:0
+    },
+    {
+      id:2,
+      title:"Iphone 11",
+      price:150,
+      quantity:1,
+      like:0
+    },
+    {
+      id:3,
+      title:"Iphone X",
+      price:100,
+      quantity:3,
+      like:0
+    },
+    {
+      id:4,
+      title:"Iphone 8",
+      price:120,
+      quantity:1,
+      like:0
+    },
+    {
+      id:5,
+      title:"Iphone 7",
+      price:180,
+      quantity:1,
+      like:0
+    },
+  ]
+}
+alert(){
+  alert('hello')
+}
+increment(i:number){
+  this.listProduct[i].like++;
+
+}
+buy(i:number){
+  if(this.listProduct[i].quantity>0)
+  this.listProduct[i].quantity--;
+
+}
+}
